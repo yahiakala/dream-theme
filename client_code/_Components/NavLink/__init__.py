1c58bb346b31c47db39bb7da3984be1a609992ef
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2025-02-04 18:44:01 +0000

    Edited Demo.Dream1 and _Components.NavigationLink; Deleted _Components.NavigationLink_copy, _Components.TestForm and _Components.NavLink

--- client_code/_Components/NavLink/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._anvil_designer import NavLinkTemplate
-class NavLink(NavLinkTemplate):
-    def __init__(self, **properties):
-        self.init_components(**properties)
--- client_code/_Components/TestForm/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._anvil_designer import TestFormTemplate
-class TestForm(TestFormTemplate):
-    def __init__(self, **properties):
-        self.init_components(**properties)
