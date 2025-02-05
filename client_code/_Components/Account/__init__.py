cf6b5b8bb2d418e0869e837973b62f5329c5e23a
yahiakala <24422258+yahiakala@users.noreply.github.com>
2025-02-05 00:00:16 -0500

    account component

--- client_code/_Components/Account/__init__.py
+++ client_code/_Components/Account/__init__.py
@@ -1,4 +1,35 @@
 from ._anvil_designer import AccountTemplate
+from anvil.js import window
+from ..._utils.properties import innerHTML_property
+
 class Account(AccountTemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
+        self.dom_nodes['account-expand'].addEventListener('click', self._handle_click)
+
+    def _handle_click(self, e):
+        self.raise_event('click')
+
+    @property
+    def username(self):
+        return self.dom_nodes['account-username'].innerText
+
+    @username.setter
+    def username(self, value):
+        self.dom_nodes['account-username'].innerText = value
+
+    @property
+    def plan(self):
+        return self.dom_nodes['account-plan'].innerText
+
+    @plan.setter
+    def plan(self, value):
+        self.dom_nodes['account-plan'].innerText = value
+
+    @property
+    def avatar_text(self):
+        return self.dom_nodes['account-avatar'].innerText
+
+    @avatar_text.setter
+    def avatar_text(self, value):
+        self.dom_nodes['account-avatar'].innerText = value
